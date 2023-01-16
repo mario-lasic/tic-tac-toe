48a9a4dbc70d3eb0322e4f4163e8bc47587bc243
Mario Lasić <mario.lasic@outlook.com>
2023-01-16 10:57:03 +0100

    Added player logic

--- tictactoe.py
+++ tictactoe.py
@@ -19,8 +19,36 @@ gridString = """
 fields = []
 for x in grid.values():
     fields.append(x)
+legalMoves = True
 
 print("WELCOME TO GAME OF TIC-TAC-TOE!")
 print("\nTo play select number you wish to replace with your simbol!")
 
 print(gridString.format(*fields))
+
+player1Name = ""
+player2Name = ""
+
+player1Name = input("Player 1 name:")
+player2Name = input("Player 2 name:")
+
+while legalMoves:
+    movesMade = 0
+    i = 1
+    if i == 1: 
+        field = input("{}, chose number beetween 1-9:".format(player1Name))
+        if int(field) > 0 and int(field) < 10:
+            grid[field] = "X"
+            i = 2
+            movesMade +1
+        else:
+            while True:
+                field = input("{}, number must be beetween 1-9:".format(player1Name))
+                if int(field) > 0 and int(field) < 10:
+                    grid[field] = "X"
+                    i = 2
+                    movesMade +1
+                    break
+    print(i)
+    break           
+    
