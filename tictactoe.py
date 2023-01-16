26425b874d1f815f78f849fe7a51802d94c570d4
Mario Lasić <mario.lasic@outlook.com>
2023-01-16 10:35:56 +0100

    Added welcome message and instruction how to play

--- tictactoe.py
+++ tictactoe.py
@@ -1,13 +1,13 @@
 grid = {
-    "1": " ",
-    "2": " ",
-    "3": " ",
-    "4": " ",
-    "5": " ",
-    "6": " ",
-    "7": " ",
-    "8": " ",
-    "9": " ",
+    "1": "1",
+    "2": "2",
+    "3": "3",
+    "4": "4",
+    "5": "5",
+    "6": "6",
+    "7": "7",
+    "8": "8",
+    "9": "9",
 }
 
 gridString = """
@@ -16,4 +16,11 @@ gridString = """
 | {} | {} | {}
     """
 
-print(gridString)
+fields = []
+for x in grid.values():
+    fields.append(x)
+
+print("WELCOME TO GAME OF TIC-TAC-TOE!")
+print("\nTo play select number you wish to replace with your simbol!")
+
+print(gridString.format(*fields))
