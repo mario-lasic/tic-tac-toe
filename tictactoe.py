727acdd43bbc0316573be7ead886496783e178b4
Mario Lasić <mario.lasic@outlook.com>
2023-01-16 11:05:44 +0100

    Added pritingout grid logic

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,9 @@
+def printoutGrid():
+    fields = []
+    for x in grid.values():
+        fields.append(x)
+    print(gridString.format(*fields))
+
 grid = {
     "1": "1",
     "2": "2",
@@ -15,16 +21,11 @@ gridString = """
 | {} | {} | {}
 | {} | {} | {}
     """
-
-fields = []
-for x in grid.values():
-    fields.append(x)
+player = 1
 legalMoves = True
 
 print("WELCOME TO GAME OF TIC-TAC-TOE!")
 print("\nTo play select number you wish to replace with your simbol!")
-
-print(gridString.format(*fields))
 
 player1Name = ""
 player2Name = ""
@@ -34,21 +35,22 @@ player2Name = input("Player 2 name:")
 
 while legalMoves:
     movesMade = 0
-    i = 1
-    if i == 1: 
+    if player == 1: 
         field = input("{}, chose number beetween 1-9:".format(player1Name))
         if int(field) > 0 and int(field) < 10:
             grid[field] = "X"
-            i = 2
+            player = 2
             movesMade +1
+            printoutGrid()
         else:
             while True:
                 field = input("{}, number must be beetween 1-9:".format(player1Name))
                 if int(field) > 0 and int(field) < 10:
                     grid[field] = "X"
-                    i = 2
+                    player = 2
                     movesMade +1
+                    printoutGrid()
                     break
-    print(i)
     break           
+    
     
