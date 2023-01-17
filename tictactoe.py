97bc4ee222f0d0428c1e1143fb906767bbf9e374
Mario Lasić <mario.lasic@outlook.com>
2023-01-17 10:47:43 +0100

    Added try block to catch bugs

--- tictactoe.py
+++ tictactoe.py
@@ -21,25 +21,50 @@ gridString = """
 | {} | {} | {}
 | {} | {} | {}
     """
-player = 1
 legalMoves = True
-movesMade = 0
 def checkIfLegal(nm):
     if nm > 9 or nm < 0: 
         return False
     else:
         return True
-def playerTurn(pn):
+def playerTurn():
+    movesMade = 1
+    intField = int
+    player = 1
     legal = True
     while(legal):
         if player == 1: 
             field = input("{}, chose number beetween 1-9:".format(player1Name))
-            if int(field) > 0 and int(field) < 10:
-                grid[field] = "X"
-                player = 2
-                movesMade +1
-                checkIfLegal(movesMade)
-                printoutGrid()
+            while True:
+                try: 
+                    intField = int(field)
+                    break
+                except ValueError:
+                    field = input("{}, You must chose beetween 1-9: ".format(player1Name))
+            
+            print(intField)
+            allGood = True
+            while(allGood):
+                while(intField <= 0 and intField > 9):
+                    field = input("{}, you must chose number beetween 1-9: ".format(player1Name))
+                if (intField > 0 and intField <= 9):
+                    print("Here")
+                    if player == 1:
+                        while(grid[field] == "X"):
+                            try:
+                                field = input("{}, please chose another number beetween 1-9: that one is already used:".format(player1Name))
+                                grid[field]
+                            except:
+                                field = input("{}, please chose another number beetween 1-9: that one is already used:".format(player1Name))
+                    allGood = False
+                else:
+                    allGood = False
+
+            grid[field] = "X"
+            #player = 2
+            movesMade = movesMade + 1
+            legal = checkIfLegal(movesMade)
+            printoutGrid()
 
     
 
@@ -52,3 +77,5 @@ player2Name = ""
 
 player1Name = input("Player 1 name:")
 player2Name = input("Player 2 name:")
+
+playerTurn()
